9f17f49998fe20f8711169e194b45a1a4286be72
aggre <hiroyuki.aggre@gmail.com>
2018-01-09 18:40:51 +0900

    Add oo-offer element

---
+++ src/elements/oo-offer/index.ts
@@ -0,0 +1,39 @@
+import {html} from 'lit-html'
+import render from '../../lib/render'
+
+const ATTR = {
+	DATA_IAM: 'data-iam'
+}
+
+export default class extends HTMLElement {
+	state: {
+		iam: string
+	}
+
+	static get observedAttributes() {
+		return [ATTR.DATA_IAM]
+	}
+
+	constructor() {
+		super()
+		const iam = this.getAttribute(ATTR.DATA_IAM)
+		this.state = {iam}
+		this.render()
+	}
+
+	attributeChangedCallback(attr, prev, next) {
+		this.state.iam = next
+		this.render()
+	}
+
+	html(iam) {
+		return html`
+		<style>
+		</style>
+		`
+	}
+
+	render() {
+		render(this.html(this.state.iam), this)
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import define from './lib/define'
 import ooButton from './elements/oo-button'
 import ooModal from './elements/oo-modal'
+import ooOffer from './elements/oo-offer'
 
 define('oo-button', ooButton)
 define('oo-modal', ooModal)
+define('oo-offer', ooOffer)
