23f24cb062b7b5e068b15f064440aa23ce8e1631
aggre <hiroyuki.aggre@gmail.com>
2018-01-09 17:30:01 +0900

    Add test cases for oo-modal

--- src/elements/oo-modal/index.test.ts
+++ src/elements/oo-modal/index.test.ts
@@ -3,6 +3,7 @@ import define from '../../lib/define'
 import insertElement from '../../lib/test/insert-element'
 import getElement from '../../lib/test/get-element'
 import removeElement from '../../lib/test/remove-element'
+const {document} = window
 
 const ELEMENT = 'oo-modal'
 
@@ -14,9 +15,40 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		insertElement(ELEMENT)
 		expect(getElement(ELEMENT)[0]).to.be.ok()
 	})
-	it('"body" slot shows inner elements of modal')
-	it('Open this modal')
-	it('Close this modal')
+	it('"header" slot shows heading elements of modal', () => {
+		removeElement(ELEMENT)
+		document.body.insertAdjacentHTML('afterbegin', `
+		<oo-modal>
+			<div slot="header">Title</div>
+		</oo-modal>`)
+		const element = getElement(ELEMENT)[0]
+		const slotHeader: HTMLSlotElement = element.shadowRoot.querySelector('slot[name=header]')
+		const assigned = slotHeader.assignedNodes()
+		expect(assigned[0].textContent).to.be('Title')
+	})
+	it('"body" slot shows inner elements of modal', () => {
+		removeElement(ELEMENT)
+		document.body.insertAdjacentHTML('afterbegin', `
+		<oo-modal>
+			<div slot="body">The content</div>
+		</oo-modal>`)
+		const element = getElement(ELEMENT)[0]
+		const slotHeader: HTMLSlotElement = element.shadowRoot.querySelector('slot[name=body]')
+		const assigned = slotHeader.assignedNodes()
+		expect(assigned[0].textContent).to.be('The content')
+	})
+	it('Open this modal', () => {
+		const element = getElement(ELEMENT)[0]
+		element.setAttribute('data-open', 'enabled')
+		const modal = element.shadowRoot.querySelector('.modal')
+		expect(modal.clientHeight).to.be.above(0)
+	})
+	it('Close this modal', () => {
+		const element = getElement(ELEMENT)[0]
+		element.setAttribute('data-open', 'disabled')
+		const modal = element.shadowRoot.querySelector('.modal')
+		expect(modal.clientHeight).to.be(0)
+	})
 	after(() => {
 		removeElement(ELEMENT)
 	})
