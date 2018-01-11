6a7d9fbeb892965b96ef6a5ab8d3b9c3b9f5f01c
aggre <hiroyuki.aggre@gmail.com>
2018-01-11 17:39:50 +0900

    Add oo-profile element

--- src/elements/oo-profile/index.ts
+++ src/elements/oo-profile/index.ts
@@ -1,11 +1,5 @@
 import {html} from 'lit-html'
 import render from '../../lib/render'
-import profile from '../oo-profile'
-import signIn from '../oo-sign-in'
-import define from '../../lib/define'
-
-define('oo-profile', profile)
-define('oo-sign-in', signIn)
 
 const ATTR = {
 	DATA_IAM: 'data-iam'
@@ -32,27 +26,18 @@ export default class extends HTMLElement {
 		this.render()
 	}
 
-	html(iam: string) {
+	html(iam) {
 		return html`
 		<style>
 			:host {
 				display: block;
 			}
 		</style>
-		<oo-profile data-iam$=${iam}></oo-profile>
-		<oo-sign-in on-signedin=${e => this.onSignedIn(e)} on-signedinerror=${e => this.onSignedInError(e)}></oo-sign-in>
+		<div></div>
 		`
 	}
 
 	render() {
 		render(this.html(this.state.iam), this)
-	}
-
-	onSignedIn(e: CustomEvent) {
-		console.log(e)
-	}
-
-	onSignedInError(e: CustomEvent) {
-		console.log(e)
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import ooButton from './elements/oo-button'
 import ooModal from './elements/oo-modal'
 import ooOffer from './elements/oo-offer'
 import ooSignIn from './elements/oo-sign-in'
+import ooProfile from './elements/oo-profile'
 
 define('oo-button', ooButton)
 define('oo-modal', ooModal)
 define('oo-offer', ooOffer)
 define('oo-sign-in', ooSignIn)
+define('oo-profile', ooProfile)
