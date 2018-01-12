804b53f8572bde7f5d095bf9e62e27bca6353aae
aggre <hiroyuki.aggre@gmail.com>
2018-01-12 18:18:17 +0900

    Add test case

--- src/elements/oo-profile/index.test.ts
+++ src/elements/oo-profile/index.test.ts
@@ -16,6 +16,44 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		expect(getElement(ELEMENT)[0]).to.be.ok()
 	})
 
+	it('Display OO user information specified by "data-iam" attribute', done => {
+		removeElement(ELEMENT)
+		const callback = () => {
+			const element = getElement(ELEMENT)[0]
+			expect(element.shadowRoot.querySelector('.name').textContent).to.be('test')
+			expect(element.shadowRoot.querySelector('.photo').getAttribute('style')).to.be('background-image: url(https://example.com/img.jpg)')
+			Array.prototype.forEach.call(element.shadowRoot.querySelectorAll('.skill'), i => {
+				expect(i.textContent).to.be('test')
+			})
+			element.removeEventListener('userupdated', callback)
+			done()
+		}
+		insertElement(ELEMENT, new Map([['data-iam', 'test']])).addEventListener('userupdated', callback)
+	})
+
+	it('Display empty when there is a User UID that does not exist', done => {
+		removeElement(ELEMENT)
+		const callback = () => {
+			const element = getElement(ELEMENT)[0]
+			expect(element.shadowRoot.querySelector('.name').textContent).to.be('')
+			expect(element.shadowRoot.querySelector('.photo').getAttribute('style')).to.be('background-image: url()')
+			expect(element.shadowRoot.querySelectorAll('.skill')).to.have.length(0)
+			element.removeEventListener('userupdated', callback)
+			done()
+		}
+		insertElement(ELEMENT, new Map([['data-iam', 'xxx']])).addEventListener('userupdated', callback)
+	})
+
+	it('Update when "data-iam" attribute is changed', done => {
+		const element = getElement(ELEMENT)[0]
+		const callback = () => {
+			element.removeEventListener('userupdated', callback)
+			done()
+		}
+		element.addEventListener('userupdated', callback)
+		element.setAttribute('data-iam', 'yyy')
+	})
+
 	after(() => {
 		removeElement(ELEMENT)
 	})
