765aee6555e8611ae97964346b2ec96c616aa62b
aggre <hiroyuki.aggre@gmail.com>
2018-01-15 14:22:58 +0900

    Add test case

--- src/elements/oo-ask/index.test.ts
+++ src/elements/oo-ask/index.test.ts
@@ -16,6 +16,18 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		expect(getElement(ELEMENT)[0]).to.be.ok()
 	})
 
+	it('Display OO user pricing specified by "data-iam" attribute')
+
+	it('Display empty when there is a User UID that does not exist')
+
+	it('Update when "data-iam" attribute is changed')
+
+	it('Sign in by click button')
+
+	it('If signing in for the first time, register new profile')
+
+	it('Dispatch the contents of the question are "asked" event')
+
 	after(() => {
 		removeElement(ELEMENT)
 	})
--- src/elements/oo-ask/index.ts
+++ src/elements/oo-ask/index.ts
@@ -1,11 +1,18 @@
 import {html} from 'lit-html'
 import render from '../../lib/render'
+import getUser from '../../lib/oo-api-get-user'
+import isSuccess from '../../lib/is-api-success'
+import toMap from '../../lib/extensions-to-map'
 
 const ATTR = {
 	DATA_IAM: 'data-iam'
 }
+const EVENT = {
+	USER_UPDATED: new Event('userupdated')
+}
 
 const iam: WeakMap<object, string> = new WeakMap()
+const pricePerHour: WeakMap<object, number> = new WeakMap()
 
 export default class extends HTMLElement {
 	static get observedAttributes() {
@@ -20,20 +27,33 @@ export default class extends HTMLElement {
 
 	attributeChangedCallback(attr, prev, next) {
 		iam.set(this, next)
-		this.render()
+		this.fetchUserData()
 	}
 
-	html(uid: string) {
+	connectedCallback() {
+		this.fetchUserData()
+	}
+
+	html(p: number) {
 		return html`
 		<style>
-			:host {
-				display: block;
-			}
 		</style>
 		`
 	}
 
 	render() {
-		render(this.html(iam.get(this)), this)
+		render(this.html(pricePerHour.get(this)), this)
+	}
+
+	async fetchUserData() {
+		const res = await getUser(iam.get(this))
+		if (isSuccess(res.status) && Array.isArray(res.response)) {
+			const ext = toMap(res.response)
+			pricePerHour.set(this, ext.get('price_per_hour'))
+			this.render()
+		} else {
+			this.render()
+		}
+		this.dispatchEvent(EVENT.USER_UPDATED)
 	}
 }
