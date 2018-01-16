ac5ad9d5625988f14b27b2162b81259bfff9400f
aggre <hiroyuki.aggre@gmail.com>
2018-01-16 12:20:15 +0900

    Add oo-organisms-offer-profile element

---
+++ src/elements/_organisms/oo-organisms-offer-profile/index.test.ts
@@ -0,0 +1,28 @@
+import el from './index'
+import define from '../../../lib/define'
+import insertElement from '../../../lib/test/insert-element'
+import getElement from '../../../lib/test/get-element'
+import removeElement from '../../../lib/test/remove-element'
+
+const ELEMENT = 'oo-organisms-offer-profile'
+
+describe(`<${ELEMENT}></${ELEMENT}>`, () => {
+	before(() => {
+		define(ELEMENT, el)
+	})
+
+	it('Mount on document', () => {
+		insertElement(ELEMENT)
+		expect(getElement(ELEMENT)[0]).to.be.ok()
+	})
+
+	it('Forward "data-iam" attribute to <oo-profile> and <oo-ask>')
+
+	it('Forward "changed" event of <oo-ask> as "askchanged"')
+
+	it('Dispatch "next" event when button clicked')
+
+	after(() => {
+		removeElement(ELEMENT)
+	})
+})
---
+++ src/elements/_organisms/oo-organisms-offer-profile/index.ts
@@ -0,0 +1,75 @@
+import {html} from 'lit-html'
+import render from '../../../lib/render'
+import profile from '../../oo-profile'
+import ask from '../../oo-ask'
+import define from '../../../lib/define'
+
+define('oo-profile', profile)
+define('oo-ask', ask)
+
+const ATTR = {
+	DATA_IAM: 'data-iam'
+}
+const EVENT = {
+	ASK_CHANGED: detail => new CustomEvent('askchanged', {detail}),
+	NEXT: new Event('next')
+}
+
+const iam: WeakMap<object, string> = new WeakMap()
+
+export default class extends HTMLElement {
+	static get observedAttributes() {
+		return [ATTR.DATA_IAM]
+	}
+
+	attributeChangedCallback(attr, prev, next) {
+		iam.set(this, next)
+		this.render()
+	}
+
+	html(uid: string) {
+		if (uid === undefined) {
+			return html``
+		}
+		return html`
+		<style>
+			:host {
+				display: block;
+			}
+			.container {
+				display: flex;
+			}
+			.column {
+				display: flex;
+				width: 50%;
+				justify-content: flex-start;
+				align-items: center;
+				&.ask {
+					flex-direction: column;
+				}
+			}
+		</style>
+		<div class=container>
+			<div class='column profile'>
+				<oo-profile data-iam$='${uid}'></oo-profile>
+			</div>
+			<div class='column ask'>
+				<oo-ask data-iam$='${uid}' on-changed='${e => this.onAskChanged(e)}'></oo-ask>
+				<button on-click='${() => this.onNextClick()}'>Next</button>
+			</div>
+		</div>
+		`
+	}
+
+	render() {
+		render(this.html(iam.get(this)), this)
+	}
+
+	onAskChanged(e: CustomEvent) {
+		this.dispatchEvent(EVENT.ASK_CHANGED(e.detail))
+	}
+
+	onNextClick() {
+		this.dispatchEvent(EVENT.NEXT)
+	}
+}
