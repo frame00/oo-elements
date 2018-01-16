5ec6196267b0b935acf5536d7b34cde677fe1a8a
aggre <hiroyuki.aggre@gmail.com>
2018-01-16 12:41:00 +0900

    Add oo-organisms-offer-sign-in elemenet

---
+++ src/elements/_organisms/oo-organisms-offer-sign-in/index.test.ts
@@ -0,0 +1,26 @@
+import el from './index'
+import define from '../../../lib/define'
+import insertElement from '../../../lib/test/insert-element'
+import getElement from '../../../lib/test/get-element'
+import removeElement from '../../../lib/test/remove-element'
+
+const ELEMENT = 'oo-organisms-offer-sign-in'
+
+describe(`<${ELEMENT}></${ELEMENT}>`, () => {
+	before(() => {
+		define(ELEMENT, el)
+	})
+
+	it('Mount on document', () => {
+		insertElement(ELEMENT)
+		expect(getElement(ELEMENT)[0]).to.be.ok()
+	})
+
+	it('Forward "signedin" event of <oo-sign-in>')
+
+	it('Forward "signedinerror" event of <oo-sign-in>')
+
+	after(() => {
+		removeElement(ELEMENT)
+	})
+})
---
+++ src/elements/_organisms/oo-organisms-offer-sign-in/index.ts
@@ -0,0 +1,43 @@
+import {html} from 'lit-html'
+import render from '../../../lib/render'
+import signIn from '../../oo-sign-in'
+import define from '../../../lib/define'
+
+define('oo-sign-in', signIn)
+
+const EVENT = {
+	SIGNED_IN: detail => new CustomEvent('signedin', {detail}),
+	SIGNED_IN_ERROR: detail => new CustomEvent('signedinerror', {detail})
+}
+
+export default class extends HTMLElement {
+	constructor() {
+		super()
+		this.render()
+	}
+
+	html() {
+		return html`
+		<style>
+			:host {
+				display: block;
+			}
+		</style>
+		<oo-sign-in data-provider='google' on-signedin='${e => this.onSignedIn(e)}' on-signedinerror='${e => this.onSignedInError(e)}'></oo-sign-in>
+		<oo-sign-in data-provider='facebook' on-signedin='${e => this.onSignedIn(e)}' on-signedinerror='${e => this.onSignedInError(e)}'></oo-sign-in>
+		<oo-sign-in data-provider='github' on-signedin='${e => this.onSignedIn(e)}' on-signedinerror='${e => this.onSignedInError(e)}'></oo-sign-in>
+		`
+	}
+
+	render() {
+		render(this.html(), this)
+	}
+
+	onSignedIn(e: CustomEvent) {
+		this.dispatchEvent(EVENT.SIGNED_IN(e.detail))
+	}
+
+	onSignedInError(e: CustomEvent) {
+		this.dispatchEvent(EVENT.SIGNED_IN_ERROR(e.detail))
+	}
+}
