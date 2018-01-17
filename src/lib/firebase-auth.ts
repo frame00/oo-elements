f310c9eebeb35641fb6ba10823d3ba4d45d368ab
aggre <hiroyuki.aggre@gmail.com>
2018-01-17 17:37:01 +0900

    Fix bug due to updating to Firebase@4.8.2

--- src/lib/firebase-auth.ts
+++ src/lib/firebase-auth.ts
@@ -1,37 +1,34 @@
-import * as f from 'firebase'
+import {initializeApp, auth} from 'firebase'
 import {GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider} from '@firebase/auth-types'
 import {AuthProvider} from '../d/auth-provider'
 import {AuthResult} from '../d/auth-result'
 import config from '../conf/firebase'
-import {FirebaseNamespace} from '@firebase/app-types'
 
 const state = {
 	initialized: false
 }
-const initialize = (app: FirebaseNamespace): FirebaseNamespace => {
+const initialize = (): void => {
 	if (state.initialized === false) {
-		const {initializeApp} = app
 		initializeApp(config)
 		state.initialized = true
 	}
-	return app
 }
 
 export default async (authProvider: AuthProvider): Promise<AuthResult> => {
-	const firebase = initialize(f)
+	initialize()
 	const provider: GoogleAuthProvider | FacebookAuthProvider | GithubAuthProvider = (prov => {
 		switch(prov) {
 			case 'google':
-				return new firebase.auth.GoogleAuthProvider()
+				return new auth.GoogleAuthProvider()
 			case 'facebook':
-				return new firebase.auth.FacebookAuthProvider()
+				return new auth.FacebookAuthProvider()
 			case 'github':
-				return new firebase.auth.GithubAuthProvider()
+				return new auth.GithubAuthProvider()
 			default:
-				return new firebase.auth.GoogleAuthProvider()
+				return new auth.GoogleAuthProvider()
 		}
 	})(authProvider)
 
-	const result: AuthResult = await firebase.auth().signInWithPopup(provider)
+	const result: AuthResult = await auth().signInWithPopup(provider)
 	return result
 }
