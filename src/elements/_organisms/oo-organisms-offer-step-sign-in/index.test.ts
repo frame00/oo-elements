6a72f9d10112aa4ae0a746252ad02d0c933f4dac
aggre <hiroyuki.aggre@gmail.com>
2018-01-17 16:50:36 +0900

    Add test for <oo-organisms-offer-step-sign-in>

--- src/elements/_organisms/oo-organisms-offer-step-sign-in/index.test.ts
+++ src/elements/_organisms/oo-organisms-offer-step-sign-in/index.test.ts
@@ -3,6 +3,7 @@ import define from '../../../lib/define'
 import insertElement from '../../../lib/test/insert-element'
 import getElement from '../../../lib/test/get-element'
 import removeElement from '../../../lib/test/remove-element'
+import event from '../../../lib/test/event'
 
 const ELEMENT = 'oo-organisms-offer-step-sign-in'
 
@@ -11,14 +12,32 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		define(ELEMENT, el)
 	})
 
+	beforeEach(() => {
+		removeElement(ELEMENT)
+	})
+
 	it('Mount on document', () => {
 		insertElement(ELEMENT)
 		expect(getElement(ELEMENT)[0]).to.be.ok()
 	})
 
-	it('Forward "signedin" event of <oo-sign-in>')
+	it('Forward "signedin" event of <oo-sign-in>', done => {
+		const element = insertElement(ELEMENT)
+		element.addEventListener('signedin', (e: CustomEvent) => {
+			expect(e.detail.test).to.be(1)
+			done()
+		}, {once: true})
+		event(element.shadowRoot.querySelector('oo-sign-in'), 'signedin', {test: 1})
+	})
 
-	it('Forward "signedinerror" event of <oo-sign-in>')
+	it('Forward "signedinerror" event of <oo-sign-in>', done => {
+		const element = insertElement(ELEMENT)
+		element.addEventListener('signedinerror', (e: CustomEvent) => {
+			expect(e.detail.test).to.be(1)
+			done()
+		}, {once: true})
+		event(element.shadowRoot.querySelector('oo-sign-in'), 'signedinerror', {test: 1})
+	})
 
 	after(() => {
 		removeElement(ELEMENT)
