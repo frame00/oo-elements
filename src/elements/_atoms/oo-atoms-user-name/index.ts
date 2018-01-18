66db8c983edf9aa8c0d640d2df2a648a3ae27e57
aggre <hiroyuki.aggre@gmail.com>
2018-01-18 21:12:06 +0900

    Remove user name when mine message

--- src/elements/_atoms/oo-atoms-user-name/index.ts
+++ src/elements/_atoms/oo-atoms-user-name/index.ts
@@ -4,26 +4,22 @@ import getUser from '../../../lib/oo-api-get-user'
 import toMap from '../../../lib/extensions-to-map'
 
 type Size = 'small' | 'medium'
-type Position = 'left' | 'right'
 interface HTMLOptions {
 	iam: string,
 	name: string,
 	photo: string,
-	size: Size,
-	position: Position
+	size: Size
 }
 
 const ATTR = {
 	DATA_IAM: 'data-iam',
-	DATA_SIZE: 'data-size',
-	DATA_POSITION: 'data-position'
+	DATA_SIZE: 'data-size'
 }
 
 const iam: WeakMap<object, string> = new WeakMap()
 const name: WeakMap<object, string> = new WeakMap()
 const photo: WeakMap<object, string> = new WeakMap()
 const size: WeakMap<object, Size> = new WeakMap()
-const position: WeakMap<object, Position> = new WeakMap()
 
 const asValidSize = (data: string): Size => {
 	if(data === 'small' || data === 'medium') {
@@ -31,16 +27,10 @@ const asValidSize = (data: string): Size => {
 	}
 	return 'medium'
 }
-const asValidPosition = (data: string): Position => {
-	if (data === 'left' || data === 'right') {
-		return data
-	}
-	return 'left'
-}
 
 export default class extends HTMLElement {
 	static get observedAttributes() {
-		return [ATTR.DATA_IAM, ATTR.DATA_SIZE, ATTR.DATA_POSITION]
+		return [ATTR.DATA_IAM, ATTR.DATA_SIZE]
 	}
 
 	constructor() {
@@ -59,9 +49,6 @@ export default class extends HTMLElement {
 			case ATTR.DATA_SIZE:
 				size.set(this, asValidSize(next))
 				break
-			case ATTR.DATA_POSITION:
-				position.set(this, asValidPosition(next))
-				break
 			default:
 				break
 		}
@@ -69,7 +56,7 @@ export default class extends HTMLElement {
 	}
 
 	html(options: HTMLOptions) {
-		const {iam: uid, name: n, photo: img, size: s, position: posi} = options
+		const {iam: uid, name: n, photo: img, size: s} = options
 		return html`
 		<style>
 			@import '../../../style/_vars-font-family.css';
@@ -133,30 +120,9 @@ export default class extends HTMLElement {
 						border-radius: 10px;
 					}
 				}
-				&.right {
-					a {
-						display: flex;
-						flex-direction: row-reverse;
-					}
-					.name {
-						text-align: right;
-					}
-					&.small {
-						.name {
-							margin-left: 0;
-							margin-right: 0.5rem;
-						}
-					}
-					&.medium {
-						.name {
-							margin-left: 0;
-							margin-right: 1rem;
-						}
-					}
-				}
 			}
 		</style>
-		<header class$='${s} ${posi}'>
+		<header class$='${s}'>
 			<a href$='https://ooapp.co/${uid}'>
 				<div class=photo style$='background-image: url(${img})'></div>
 				<p class$='name ${n ? '' : 'empty'}'>${n}</p>
@@ -170,8 +136,7 @@ export default class extends HTMLElement {
 			iam: iam.get(this),
 			name: name.get(this),
 			photo: photo.get(this),
-			size: size.get(this),
-			position: position.get(this)
+			size: size.get(this)
 		}
 		render(this.html(opts), this)
 	}
--- src/elements/oo-project-messages/index.ts
+++ src/elements/oo-project-messages/index.ts
@@ -71,14 +71,17 @@ export default class extends HTMLElement {
 			const lines = lineBreak(mes.ext.get('body'))
 			const author = mes.ext.get('author')
 			const position = author === user ? 'right' : 'left'
+			const footer = author === user ? html`` :
+				html`
+				<footer slot=footer>
+					<oo-atoms-user-name data-iam$='${author}' data-size=small></oo-atoms-user-name>
+				</footer>`
 			return html`
 			<oo-atoms-message data-tooltip-position$='${position}'>
 				<section slot=body>
 					${repeat(lines, line => html`<p>${line}</p>`)}
 				</section>
-				<footer slot=footer>
-					<oo-atoms-user-name data-iam$='${author}' data-position$='${position}' data-size=small></oo-atoms-user-name>
-				</footer>
+				${footer}
 			</oo-atoms-message>
 			`
 		})}
