641b113546e0a05b9ce4002695111d634e3894fb
aggre <hiroyuki.aggre@gmail.com>
2018-01-18 11:05:22 +0900

    Add oo-project element

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import ooOffer from './elements/oo-offer'
 import ooSignIn from './elements/oo-sign-in'
 import ooProfile from './elements/oo-profile'
 import ooAsk from './elements/oo-ask'
+import ooProject from './elements/oo-project'
 
 define('oo-button', ooButton)
 define('oo-modal', ooModal)
@@ -12,3 +13,4 @@ define('oo-offer', ooOffer)
 define('oo-sign-in', ooSignIn)
 define('oo-profile', ooProfile)
 define('oo-ask', ooAsk)
+define('oo-project', ooProject)
