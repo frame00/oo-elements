ef39a511b8fe18075d6a04eae227cf3eb3df865b
aggre <hiroyuki.aggre@gmail.com>
2018-01-22 11:42:02 +0900

    Add test oo-atoms-message

--- src/elements/_atoms/oo-atoms-message/index.test.ts
+++ src/elements/_atoms/oo-atoms-message/index.test.ts
@@ -16,7 +16,53 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		expect(getElement(ELEMENT)[0]).to.be.ok()
 	})
 
-	it('')
+	it('"body" slot show as <section> inner', () => {
+		removeElement(ELEMENT)
+		document.body.insertAdjacentHTML('afterbegin', `
+		<${ELEMENT}>
+			<div slot=body>Body</div>
+		</${ELEMENT}>`)
+		const element = getElement(ELEMENT)[0]
+		const slot: HTMLSlotElement = element.shadowRoot.querySelector('slot[name="body"]')
+		const assigned = slot.assignedNodes()
+		expect(assigned[0].textContent).to.be('Body')
+	})
+
+	it('"footer" slot show as <footer> inner', () => {
+		removeElement(ELEMENT)
+		document.body.insertAdjacentHTML('afterbegin', `
+		<${ELEMENT}>
+			<div slot=footer>Footer</div>
+		</${ELEMENT}>`)
+		const element = getElement(ELEMENT)[0]
+		const slot: HTMLSlotElement = element.shadowRoot.querySelector('slot[name="footer"]')
+		const assigned = slot.assignedNodes()
+		expect(assigned[0].textContent).to.be('Footer')
+	})
+
+	describe('Specify the display position with "data-tooltip-position" attribute', () => {
+		it('left', () => {
+			const element = getElement(ELEMENT)[0]
+			element.setAttribute('data-tooltip-position', 'left')
+			const {classList} = element.shadowRoot.querySelector('main')
+			expect(classList.contains('left')).to.be.ok()
+		})
+
+		it('right', () => {
+			const element = getElement(ELEMENT)[0]
+			element.setAttribute('data-tooltip-position', 'right')
+			const {classList} = element.shadowRoot.querySelector('main')
+			expect(classList.contains('right')).to.be.ok()
+		})
+
+		it('Other as left', () => {
+			const element = getElement(ELEMENT)[0]
+			element.setAttribute('data-tooltip-position', 'xxx')
+			const {classList} = element.shadowRoot.querySelector('main')
+			expect(classList.contains('xxx')).to.be(false)
+			expect(classList.contains('left')).to.be.ok()
+		})
+	})
 
 	after(() => {
 		removeElement(ELEMENT)
