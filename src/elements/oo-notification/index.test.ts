1178653d8cca977611c195f56fd647cfe0096455
aggre <hiroyuki.aggre@gmail.com>
2018-01-29 19:11:10 +0900

    Add test case

--- src/elements/oo-notification/index.test.ts
+++ src/elements/oo-notification/index.test.ts
@@ -3,8 +3,8 @@ import define from '../../lib/define'
 import insertElement from '../../lib/test/insert-element'
 import getElement from '../../lib/test/get-element'
 import removeElement from '../../lib/test/remove-element'
-import sleep from '../../lib/test/sleep'
 import event from '../../lib/test/event'
+import sleep from '../../lib/test/sleep'
 
 const ELEMENT = 'oo-notification'
 
@@ -13,14 +13,46 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		define(ELEMENT, el)
 	})
 
+	beforeEach(() => {
+		removeElement(ELEMENT)
+	})
+
 	it('Mount on document', () => {
 		insertElement(ELEMENT)
 		expect(getElement(ELEMENT)[0]).to.be.ok()
 	})
 
-	it('Show "body" slot')
+	it('Show "body" slot', () => {
+		document.body.insertAdjacentHTML('afterbegin', `
+		<oo-notification>
+			<div slot="body">The Content</div>
+		</oo-notification>`)
+		const element = getElement(ELEMENT)[0]
+		const slotHeader: HTMLSlotElement = element.shadowRoot.querySelector('slot[name=body]')
+		const assigned = slotHeader.assignedNodes()
+		expect(assigned[0].textContent).to.be('The Content')
+	})
 
-	it('Remove self element after 3 seconds')
+	it('Remove self element when clicking <main>', async () => {
+		const element = insertElement(ELEMENT)
+		event(element.shadowRoot.querySelector('main'), 'click')
+		await sleep(50)
+		expect(getElement(ELEMENT)[0]).to.not.be.ok()
+	})
+
+	describe('Styling by "data-type" attribute', () => {
+		it('"data-type" is "error"', () => {
+			const element = insertElement(ELEMENT, new Map([['data-type', 'error']]))
+			const main = element.shadowRoot.querySelector('main')
+			expect(main.classList.toString()).to.contain('error')
+		})
+
+		it('"data-type" is undefined', () => {
+			const element = insertElement(ELEMENT, new Map([['data-type', 'xxx']]))
+			const main = element.shadowRoot.querySelector('main')
+			expect(main.classList.toString()).to.be('')
+		})
+	})
 
 	after(() => {
 		removeElement(ELEMENT)
--- src/elements/oo-notification/index.ts
+++ src/elements/oo-notification/index.ts
@@ -1,28 +1,70 @@
 import {html} from 'lit-html'
 import render from '../../lib/render'
+import weakMap from '../../lib/weak-map'
+
+type Type = 'error' | ''
+
+const ATTR = {
+	DATA_TYPE: 'data-type'
+}
+
+const stateType = weakMap<Type>()
+
+const asType = (data: string): Type => {
+	if (data === 'error' || data === '') {
+		return data
+	}
+	return ''
+}
 
 export default class extends HTMLElement {
-	constructor() {
-		super()
-		setTimeout(() => {
-			this.parentElement.removeChild(this)
-		}, 3000)
+	connectedCallback() {
+		stateType.set(this, asType(this.getAttribute(ATTR.DATA_TYPE)))
+		this.render()
 	}
 
-	html() {
+	html(type: Type) {
 		return html`
 		<style>
+			@import '../../style/_vars-input.css';
 			:host {
 				display: block;
 			}
+			main {
+				padding: 1rem;
+				z-index: 1001;
+				animation-name: showNotification;
+				animation-duration: 0.3s;
+				animation-fill-mode: forwards;
+				border-radius: 5px;
+				background: whitesmoke;
+				&.error {
+					background: var(--rejected-background);
+					color: white;
+				}
+			}
+			@keyframes showNotification {
+				from {
+					opacity: 0;
+					transform: translateY(20px);
+				}
+				to {
+					opacity: 1;
+					transform: translateY(0);
+				}
+			}
 		</style>
-		<main>
+		<main class$='${type}' on-click='${() => this.close()}'>
 			<slot name=body></slot>
 		</main>
 		`
 	}
 
 	render() {
-		render(this.html(), this)
+		render(this.html(stateType.get(this)), this)
+	}
+
+	close() {
+		this.parentElement.removeChild(this)
 	}
 }
