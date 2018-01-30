2a5e338d78b6b0c6b59be2dd9765a48fcb719425
aggre <hiroyuki.aggre@gmail.com>
2018-01-30 18:03:23 +0900

    Add test case

--- src/elements/oo-nav/index.test.ts
+++ src/elements/oo-nav/index.test.ts
@@ -3,6 +3,9 @@ import define from '../../lib/define'
 import insertElement from '../../lib/test/insert-element'
 import getElement from '../../lib/test/get-element'
 import removeElement from '../../lib/test/remove-element'
+import sleep from '../../lib/test/sleep'
+import event from '../../lib/test/event'
+const {document} = window
 
 const ELEMENT = 'oo-nav'
 
@@ -11,9 +14,88 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		define(ELEMENT, el)
 	})
 
+	beforeEach(() => {
+		removeElement(ELEMENT)
+	})
+
 	it('Mount on document', () => {
 		insertElement(ELEMENT)
 		expect(getElement(ELEMENT)[0]).to.be.ok()
+	})
+
+	describe('Show slot', () => {
+		it('Show "item" slot as nav inner contents', () => {
+			document.body.insertAdjacentHTML('afterbegin', `
+			<oo-nav>
+				<a slot=item>1</a>
+				<a slot=item>2</a>
+				<a slot=item>3</a>
+			</oo-nav>`)
+			const element = getElement(ELEMENT)[0]
+			const slotHeader: HTMLSlotElement = element.shadowRoot.querySelector('slot[name=item]')
+			const assigned = slotHeader.assignedNodes()
+			expect(assigned[0].textContent).to.be('1')
+			expect(assigned[1].textContent).to.be('2')
+			expect(assigned[2].textContent).to.be('3')
+		})
+	})
+
+	describe('Attribuets', () => {
+		it('Pass the value of "data-direction" to the <nav> element', () => {
+			const element = insertElement(ELEMENT, new Map([['data-direction', 'column']]))
+			const nav = element.shadowRoot.querySelector('nav')
+			expect(nav.classList.toString()).to.contain('column')
+		})
+
+		it('If "data-direction" is other than "column", pass "column"', () => {
+			const element = insertElement(ELEMENT, new Map([['data-direction', 'xxx']]))
+			const nav = element.shadowRoot.querySelector('nav')
+			expect(nav.classList.toString()).to.contain('column')
+
+			const element$ = insertElement(ELEMENT)
+			const nav$ = element$.shadowRoot.querySelector('nav')
+			expect(nav$.classList.toString()).to.contain('column')
+		})
+	})
+
+	describe('Toggle navigation', () => {
+		it('When ".handle" element is clicked and <nav> element has "close", "open" is added to the <nav> element.', () => {
+			const element = insertElement(ELEMENT)
+			const nav = element.shadowRoot.querySelector('nav')
+			const handle = element.shadowRoot.querySelector('.handle')
+			event(handle, 'click')
+			expect(nav.classList.toString()).to.contain('open')
+			expect(nav.classList.toString()).to.not.contain('close')
+		})
+
+		it('When ".handle" element is clicked and <nav> element has "open", "close" is added to the <nav> element.', () => {
+			const element = insertElement(ELEMENT)
+			const nav = element.shadowRoot.querySelector('nav')
+			const handle = element.shadowRoot.querySelector('.handle')
+			event(handle, 'click')
+			event(handle, 'click')
+			expect(nav.classList.toString()).to.contain('close')
+			expect(nav.classList.toString()).to.not.contain('open')
+		})
+
+		it('When ".toggle" element is clicked and <nav> element has "close", "open" is added to the <nav> element.', () => {
+			const element = insertElement(ELEMENT)
+			const nav = element.shadowRoot.querySelector('nav')
+			const toggle = element.shadowRoot.querySelector('.toggle')
+			event(toggle, 'click')
+			expect(nav.classList.toString()).to.contain('open')
+			expect(nav.classList.toString()).to.not.contain('close')
+		})
+
+		it('When ".toggle" element is clicked and <nav> element has "open", "close" is added to the <nav> element.', () => {
+			const element = insertElement(ELEMENT)
+			const nav = element.shadowRoot.querySelector('nav')
+			const toggle = element.shadowRoot.querySelector('.toggle')
+			event(toggle, 'click')
+			event(toggle, 'click')
+			expect(nav.classList.toString()).to.contain('close')
+			expect(nav.classList.toString()).to.not.contain('open')
+		})
 	})
 
 	after(() => {
--- src/elements/oo-nav/index.ts
+++ src/elements/oo-nav/index.ts
@@ -2,39 +2,137 @@ import {html} from 'lit-html'
 import render from '../../lib/render'
 import weakMap from '../../lib/weak-map'
 
+type State = 'open' | 'close'
+type Direction = 'column'
+
 const ATTR = {
-	DATA_ACTIVE: 'data-active'
+	DATA_DIRECTION: 'data-direction'
 }
 
-const stateActive = weakMap<string>()
+const state = weakMap<State>()
+const direction = weakMap<Direction>()
+
+const asDirection = (d: string): Direction => {
+	if (d === 'column') {
+		return d
+	}
+	return 'column'
+}
 
 export default class extends HTMLElement {
 	static get observedAttributes() {
-		return [ATTR.DATA_ACTIVE]
+		return [ATTR.DATA_DIRECTION]
 	}
 
 	constructor() {
 		super()
-		stateActive.set(this, this.getAttribute(ATTR.DATA_ACTIVE))
+		state.set(this, 'close')
+		direction.set(this, asDirection(this.getAttribute(ATTR.DATA_DIRECTION)))
 		this.render()
 	}
 
 	attributeChangedCallback(attr, prev, next) {
-		if (prev === next || !next) {
+		if (prev === next) {
 			return
 		}
-		stateActive.set(this, next)
+		direction.set(this, asDirection(next))
 		this.render()
 	}
 
-	html(active: string) {
+	html(stte: State, dir: Direction) {
 		return html`
 		<style>
+			@import '../../style/_reset-button.css';
+			@import '../../style/_vars-color-yellow.css';
+			:host {
+				display: block;
+			}
+			:root {
+				--handle-size: 58px;
+				--handle-margin: 1rem;
+			}
+			nav {
+				display: flex;
+				flex-wrap: wrap;
+				align-content: start;
+				width: 100%;
+				height: 100%;
+				max-height: 100%;
+				box-sizing: border-box;
+				padding: 2rem;
+				background: whitesmoke;
+				overflow: auto;
+				transition: transform 0.5s;
+				&.column {
+					justify-content: center;
+					&.close {
+						transform: translateX(-100%);
+					}
+					&.open {
+						transform: translateX(0);
+					}
+				}
+			}
+			.toggle {
+				width: 100%;
+				height: 4rem;
+				font-size: 2rem;
+			}
+			.handle {
+				position: fixed;
+				top: 0;
+				left: 0;
+				width: var(--handle-size);
+				height: var(--handle-size);
+				background: var(--yellow);
+				margin: var(--handle-margin);
+				border-radius: 50%;
+				transition: transform 0.25s;
+				&.close {
+					transform: scale(1);
+				}
+				&.open {
+					transform: scale(0);
+				}
+			}
+			::slotted(a) {
+				margin: 0 1rem;
+				padding: 1rem 0;
+				text-decoration: none;
+				color: #333;
+			}
+			::slotted(a[active]) {
+				border-bottom: 1px solid;
+			}
+			@media (min-width: 768px) {
+				nav {
+					&.column {
+						justify-content: baseline;
+						&.close {
+							transform: translateX(0);
+						}
+					}
+				}
+				.handle,
+				.toggle {
+					display: none;
+				}
+			}
 		</style>
+		<nav class$='${stte} ${dir}'>
+			<button class=toggle on-click='${() => this.onHandleClick()}'>×</button>
+			<slot name=item></slot>
+		</nav>
+		<div class$='handle ${stte}' on-click='${() => this.onHandleClick()}'></div>
 		`
 	}
 
 	render() {
-		render(this.html(stateActive.get(this)), this)
+		render(this.html(state.get(this), direction.get(this)), this)
+	}
+
+	onHandleClick() {
+		state.set(this, state.get(this) === 'close' ? 'open' : 'close')
+		this.render()
 	}
 }
