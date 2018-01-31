24f2ee1486002fbfc430a677fc35e3e18d065dd5
aggre <hiroyuki.aggre@gmail.com>
2018-01-31 12:14:43 +0900

    Add deploy script

---
+++ deploy.js
@@ -0,0 +1,28 @@
+const {
+	S3_PATH, // e.g. Bucket/Dir/Src/
+	S3_IAM_KEY,
+	S3_IAM_SECRET
+} = process.env
+
+if (!S3_PATH || !S3_IAM_KEY || !S3_IAM_SECRET) {
+	return
+}
+
+const gulp = require('gulp')
+const s3 = require('gulp-s3-upload')({
+	accessKeyId: S3_IAM_KEY,
+	secretAccessKey: S3_IAM_SECRET
+})
+
+const [Bucket] = S3_PATH.split('/')
+const keyPrefix = S3_PATH.replace(`${Bucket}/`, '')
+
+const config = {
+	Bucket,
+	ACL: 'public-read',
+	keyTransform(filename) {
+		return `${keyPrefix}${filename}`
+	}
+}
+
+gulp.src('dist/**').pipe(s3(config))
