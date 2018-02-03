fb145f4bf8537d1e61ee6b0f96cc5d6b7c021acb
aggre <hiroyuki.aggre@gmail.com>
2018-02-03 13:55:16 +0900

    Do not use ShadowDOM

--- src/elements/oo-version/index.test.ts
+++ src/elements/oo-version/index.test.ts
@@ -1,7 +1,6 @@
 import el from './index'
 import define from '../../lib/define'
 import insertElement from '../../lib/test/insert-element'
-import getElement from '../../lib/test/get-element'
 import removeElement from '../../lib/test/remove-element'
 
 const ELEMENT = 'oo-version'
@@ -13,14 +12,18 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 	})
 
 	it('Mount on document', () => {
-		insertElement(ELEMENT)
-		expect(getElement(ELEMENT)[0]).to.be.ok()
+		expect(insertElement(ELEMENT)).to.be.ok()
 	})
 
 	it('Show package version', () => {
 		const element = insertElement(ELEMENT)
-		const version = element.shadowRoot.querySelector('span')
+		const version = element.querySelector('span')
 		expect(version.textContent).to.be(PACKAGE_VERSION)
+	})
+
+	it('Not use Shadow DOM', () => {
+		const element = insertElement(ELEMENT)
+		expect(element.shadowRoot).to.not.be.ok()
 	})
 
 	after(() => {
