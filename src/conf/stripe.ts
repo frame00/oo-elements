b973bb5da5368041ad5e2fd2900e57aa79ab87ae
aggre <hiroyuki.aggre@gmail.com>
2018-02-05 13:13:28 +0900

    Stripe callback URL for production

--- src/conf/stripe.ts
+++ src/conf/stripe.ts
@@ -2,4 +2,4 @@ const {TRAVIS_BRANCH = false} = process.env
 
 export const cdn = 'https://checkout.stripe.com/checkout.js'
 export const publicKey = TRAVIS_BRANCH ? 'pk_live_9gWUVVpr9UGIaclVD8PwAIQj' : 'pk_test_mhnxgwHlCHhDGe0GIzTUNDAf'
-export const oAuthUrl = 'https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=ca_CGWAAnRV1DMThelEegomOo1sdFb96lDD&scope=read_write'
+export const oAuthUrl = TRAVIS_BRANCH ? 'https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=ca_CGWAVJ2syifI5lzc0hjMbcewKStZCrig&scope=read_write' : 'https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=ca_CGWAAnRV1DMThelEegomOo1sdFb96lDD&scope=read_write'
