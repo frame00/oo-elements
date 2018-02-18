eba52d940fe98c2f077a4b52ac969ff9f0d89695
aggre <hiroyuki.aggre@gmail.com>
2018-02-18 16:45:07 +0900

    Adjust height of <oo-ask>

--- src/elements/oo-ask-with-sign-in/index.ts
+++ src/elements/oo-ask-with-sign-in/index.ts
@@ -1,3 +1,4 @@
+import OO from '../../lib/classes/oo-element'
 import {html, render} from '../../lib/html'
 import stepSignIn from '../_organisms/oo-organisms-ask-step-sign-in'
 import askForm from '../oo-ask-form'
@@ -36,6 +37,7 @@ const stateScope = weakMap<Scope>()
 const stateCurrency = weakMap<Currency>()
 const stateAuthorized = weakMap<boolean>()
 const stateSignInFlow = weakMap<SignInFlow>()
+const statePrevActiveStep = weakMap<Element>()
 
 const validation = (el: HTMLElement): boolean => {
 	const body = stateMessage.get(el)
@@ -54,8 +56,16 @@ const asSignInFlow = (d: string): SignInFlow => {
 	}
 	return 'popup'
 }
+const fitHeight = (el: HTMLElement, target: HTMLElement): boolean => {
+	const height = el.offsetHeight
+	if (height > 0) {
+		target.style.height = `${height}px`
+		return true
+	}
+	return false
+}
 
-export default class extends HTMLElement {
+export default class extends OO {
 	static get observedAttributes() {
 		return [ATTR.DATA_IAM, ATTR.DATA_SIGN_IN_FLOW]
 	}
@@ -63,7 +73,6 @@ export default class extends HTMLElement {
 	constructor() {
 		super()
 		stateAuthorized.set(this, false)
-		this.render()
 	}
 
 	attributeChangedCallback(attr, prev, next) {
@@ -80,7 +89,18 @@ export default class extends HTMLElement {
 			default:
 				break
 		}
+		if (this.connected) {
+			this.render()
+		}
+	}
+
+	connectedCallback() {
+		super.connectedCallback()
 		this.render()
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback()
 	}
 
 	html(opts: Options, progress: boolean) {
@@ -122,10 +142,11 @@ export default class extends HTMLElement {
 				ul {
 					width: 300%;
 					display: flex;
+					align-items: flex-start;
 					margin: 0;
 					padding: 0;
 					list-style: none;
-					transition: transform 0.5s;
+					transition: transform 0.5s, height 0.5s;
 					&.ask {
 						transform: translateX(0);
 					}
@@ -168,14 +189,14 @@ export default class extends HTMLElement {
 		<oo-ask-form data-iam$='${uid ? uid : ''}' on-changed='${e => this.onAskChanged(e)}'></oo-ask-form>
 		<div class=steps>
 			<ul class$='${step}'>
-				<li class=step>
+				<li class=step active?='${step === 'ask'}'>
 					<button class=authorization on-click='${() => this.onAuthorization()}'>Authenticate</button>
 					<p class=description>Next step: Authenticate account with Google, Facebook or GitHub.</p>
 				</li>
-				<li class=step>
+				<li class=step active?='${step === 'signin'}'>
 					<oo-organisms-ask-step-sign-in class=signin data-flow$='${flow}' on-signedin='${e => this.onSignedIn(e)}'></oo-organisms-ask-step-sign-in>
 				</li>
-				<li class=step>
+				<li class=step active?='${step === 'submit'}'>
 					<button class=submit disabled?='${progress}' on-click='${() => this.createProject()}'>Ask</button>
 					<p class=description>Just send it!</p>
 				</li>
@@ -191,6 +212,24 @@ export default class extends HTMLElement {
 			sender: stateOfferer.get(this),
 			flow: stateSignInFlow.get(this)
 		}, progress), this)
+		this.renderedCallback()
+	}
+
+	async renderedCallback() {
+		const items = this.shadowRoot.querySelector('.steps ul')
+		if (items) {
+			const item = this.shadowRoot.querySelector('.step[active]')
+			if (statePrevActiveStep.get(this) !== item) {
+				statePrevActiveStep.set(this, item)
+				let fit = false
+				let count = 100
+				while(!fit && count > 0) {
+					fit = fitHeight(item as HTMLElement, items as HTMLElement)
+					count -= 1
+					await new Promise(resolve => setTimeout(resolve, 10))
+				}
+			}
+		}
 	}
 
 	onAskChanged(e: HTMLElementEventChangeAsk<HTMLElement>) {
