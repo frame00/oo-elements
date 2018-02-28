d9887d2b805e3dd9cd572eca7b9aade2a640027a
aggre <hiroyuki.aggre@gmail.com>
2018-02-28 16:46:55 +0900

    Apply new superclass: oo-atoms-badge

---
+++ src/elements/oo-element.ts
@@ -0,0 +1,37 @@
+import {render} from 'lit-html/lib/lit-extended'
+import {TemplateResult} from 'lit-html'
+import weakMap from '../lib/weak-map'
+
+const connected = weakMap<boolean>()
+
+export class OOElement extends HTMLElement {
+	get connected() {
+		return connected.get(this)
+	}
+
+	connectedCallback(update: boolean = true) {
+		connected.set(this, true)
+		if (update) {
+			this.update()
+		}
+	}
+
+	disconnectedCallback() {
+		connected.delete(this)
+	}
+
+	private _render(template: TemplateResult) {
+		render(template, this.shadowRoot || this.attachShadow({mode: 'open'}))
+	}
+
+	protected update() {
+		const template: TemplateResult = this.render()
+		if (template) {
+			this._render(template)
+		}
+	}
+
+	protected render(): TemplateResult {
+		throw new Error('Render function not Implemented')
+	}
+}
