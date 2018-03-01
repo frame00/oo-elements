d3a0ee8afa4d6dfef4c2867642f57ccea0ad40fb
aggre <hiroyuki.aggre@gmail.com>
2018-03-01 12:06:16 +0900

    Apply new superclass: oo-sign-out

--- src/elements/oo-sign-out/index.ts
+++ src/elements/oo-sign-out/index.ts
@@ -1,4 +1,5 @@
-import {html, render} from '../../lib/html'
+import {OOElement} from '../oo-element'
+import {html} from '../../lib/html'
 import define from '../../lib/define'
 import button from '../_atoms/oo-atoms-button'
 import {attach, dispatch} from '../../lib/notification'
@@ -6,6 +7,7 @@ import store from '../../lib/local-storage'
 import deleteToken from '../../lib/oo-api-delete-token'
 import isSuccess from '../../lib/is-api-success'
 import customEvent from '../../lib/custom-event'
+import weakMap from '../../lib/weak-map'
 
 define('oo-atoms-button', button)
 
@@ -13,17 +15,20 @@ const EVENT = {
 	SIGNED_OUT: customEvent('signedout')
 }
 
-export default class extends HTMLElement {
+const stateProggres = weakMap<boolean>()
+
+export default class extends OOElement {
 	constructor() {
 		super()
 		attach()
 		const {uid} = store
 		if (typeof uid === 'string' && uid !== '') {
-			this.render()
+			this.update()
 		}
 	}
 
-	html(progress: boolean) {
+	render() {
+		const progress = stateProggres.get(this)
 		return html`
 		<style>
 		</style>
@@ -33,12 +38,9 @@ export default class extends HTMLElement {
 		`
 	}
 
-	render(progress: boolean = false) {
-		render(this.html(progress), this)
-	}
-
 	async signOut() {
-		this.render(true)
+		stateProggres.set(this, true)
+		this.update()
 		const del = await deleteToken()
 		const {status} = del
 		if (isSuccess(status)) {
@@ -54,6 +56,7 @@ export default class extends HTMLElement {
 				type: 'error'
 			})
 		}
-		this.render(false)
+		stateProggres.delete(this)
+		this.update()
 	}
 }
