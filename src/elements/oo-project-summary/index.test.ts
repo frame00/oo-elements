0e98e878f29e1567a1d351ccd82e9e2a40fa936a
aggre <hiroyuki.aggre@gmail.com>
2018-03-09 12:45:35 +0900

    Add testing

--- src/elements/oo-project-summary/index.test.ts
+++ src/elements/oo-project-summary/index.test.ts
@@ -4,6 +4,8 @@ import insertElement from '../../lib/test/insert-element'
 import getElement from '../../lib/test/get-element'
 import removeElement from '../../lib/test/remove-element'
 import sleep from '../../lib/test/sleep'
+import store from '../../lib/local-storage'
+import event from '../../lib/test/event'
 
 const ELEMENT = 'oo-project-summary'
 
@@ -19,7 +21,7 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 
 	it('Display project data of UID specified by "data-uid" attribute', async () => {
 		const element = insertElement(ELEMENT, new Map([['data-uid', '79zGMA1b6q']]))
-		await sleep(100)
+		await sleep(300)
 		const slotFooter: HTMLSlotElement = element.shadowRoot.querySelector('oo-atoms-message').shadowRoot.querySelector('slot[name=footer]')
 		const [slotFooterAssigned] = slotFooter.assignedNodes()
 
@@ -30,26 +32,81 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 	})
 
 	describe('Editing post', () => {
-		it('Display "Edit" button when user who is signed in is contributor')
+		it('Display "Edit" button when user who is signed in is contributor', async () => {
+			store.uid = 'test'
+			const element = insertElement(ELEMENT, new Map([['data-uid', '79zGMA1b6q']]))
+			await sleep(300)
+			const button = element.shadowRoot.querySelector('button')
+			expect(button.textContent).to.be('Edit')
+		})
 
-		it('Clicking the sign-in button makes "oo-project-editor" a "oo-modal" body slot')
+		it('Clicking the sign-in button makes "oo-project-editor" a "oo-modal" body slot', async () => {
+			const element = insertElement(ELEMENT, new Map([['data-uid', '79zGMA1b6q']]))
+			await sleep(300)
+			const button = element.shadowRoot.querySelector('button')
+			const modal = element.shadowRoot.querySelector('oo-modal')
+			expect(modal.querySelector('oo-project-editor')).to.not.be.ok()
+			event(button, 'click')
+			expect(modal.querySelector('oo-project-editor')).to.be.ok()
+		})
 
 		describe('When "oo-project-editor" dispatches "updated" event', () => {
-			it('Close "oo-modal"')
+			it('Close "oo-modal"', async () => {
+				const element = insertElement(ELEMENT, new Map([['data-uid', '79zGMA1b6q']]))
+				await sleep(300)
+				const button = element.shadowRoot.querySelector('button')
+				const modal = element.shadowRoot.querySelector('oo-modal')
+				event(button, 'click')
+				const editor = modal.querySelector('oo-project-editor')
+				event(editor, 'updated')
+				expect(modal.getAttribute('data-open')).to.be('disabled')
+			})
 
-			it('Remove "oo-project-editor"')
+			it('Remove "oo-project-editor"', async () => {
+				const element = insertElement(ELEMENT, new Map([['data-uid', '79zGMA1b6q']]))
+				await sleep(300)
+				const button = element.shadowRoot.querySelector('button')
+				const modal = element.shadowRoot.querySelector('oo-modal')
+				event(button, 'click')
+				const editor = modal.querySelector('oo-project-editor')
+				event(editor, 'updated')
+				expect(modal.querySelector('oo-project-editor')).to.not.be.ok()
+			})
 
-			it('Display "Reload" button')
+			it('Display "Reload" button', async () => {
+				const element = insertElement(ELEMENT, new Map([['data-uid', '79zGMA1b6q']]))
+				await sleep(300)
+				const button = element.shadowRoot.querySelector('button')
+				const modal = element.shadowRoot.querySelector('oo-modal')
+				event(button, 'click')
+				const editor = modal.querySelector('oo-project-editor')
+				event(editor, 'updated')
+				const [reloadButton] = Array.from(element.shadowRoot.querySelectorAll('button'))
+				expect(reloadButton.textContent).to.be('Reload')
+			})
 		})
 
 		describe('Condition that "Edit" button is not displayed', () => {
-			it('Not signed in')
+			it('Not signed in', async () => {
+				store.clear()
+				const element = insertElement(ELEMENT, new Map([['data-uid', '79zGMA1b6q']]))
+				await sleep(300)
+				const button = element.shadowRoot.querySelector('button')
+				expect(button).to.not.be.ok()
+			})
 
-			it('Different from contributor')
+			it('Different from contributor', async () => {
+				store.uid = 'xxx'
+				const element = insertElement(ELEMENT, new Map([['data-uid', '79zGMA1b6q']]))
+				await sleep(300)
+				const button = element.shadowRoot.querySelector('button')
+				expect(button).to.not.be.ok()
+			})
 		})
 	})
 
 	after(() => {
+		store.clear()
 		removeElement(ELEMENT)
 	})
 })
