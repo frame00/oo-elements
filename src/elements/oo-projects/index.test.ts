224f24b63e7697049834ad188ab71dacaf93e8eb
aggre <hiroyuki.aggre@gmail.com>
2018-04-06 14:17:32 +0900

    Update testing

--- src/elements/oo-projects/index.test.ts
+++ src/elements/oo-projects/index.test.ts
@@ -22,8 +22,7 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		it('Fetch the related projects from "data-iam" attribute value', async () => {
 			const element: any = insertElement(ELEMENT, new Map([['data-iam', 'JEr9hsMH5m']]))
 			await sleep(300)
-			const items = element.shadowRoot.querySelectorAll('oo-atoms-message')
-			console.log('xxxx', element.projects)
+			const items = element.shadowRoot.querySelectorAll('oo-project-card')
 			expect(items).to.have.length(2)
 
 			const more = element.shadowRoot.querySelector('.paging > oo-atoms-button')
@@ -33,7 +32,7 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		it('Fetch public projects when not exists "data-iam" attribute value', async () => {
 			const element = insertElement(ELEMENT)
 			await sleep(300)
-			const items = element.shadowRoot.querySelectorAll('oo-atoms-message')
+			const items = element.shadowRoot.querySelectorAll('oo-project-card')
 			expect(items).to.have.length(2)
 
 			const more = element.shadowRoot.querySelector('.paging > oo-atoms-button')
@@ -43,7 +42,7 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		it('Fetch tagged public projects when exists "data-tag" attribute and not exists "data-iam" attribute value', async () => {
 			const element = insertElement(ELEMENT, new Map([['data-tag', 'test']]))
 			await sleep(300)
-			const items = element.shadowRoot.querySelectorAll('oo-atoms-message')
+			const items = element.shadowRoot.querySelectorAll('oo-project-card')
 			expect(items).to.have.length(2)
 
 			const more = element.shadowRoot.querySelector('.paging > oo-atoms-button')
@@ -53,8 +52,17 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		it('Show <oo-empty> when no projects exists', async () => {
 			const element: any = insertElement(ELEMENT, new Map([['data-iam', 'xxx']]))
 			await sleep(300)
-			console.log('xxxx', element.projects)
 			expect(element.shadowRoot.querySelector('oo-empty')).to.be.ok()
+		})
+	})
+
+	describe('Forward data', () => {
+		it('Forward project UID to "oo-project-card"', async () => {
+			const element = insertElement(ELEMENT, new Map([['data-iam', 'JEr9hsMH5m']]))
+			await sleep(300)
+			const items = element.shadowRoot.querySelectorAll('oo-project-card')
+			expect(items[0].getAttribute('data-uid')).to.be('79zGMA1b6q')
+			expect(items[1].getAttribute('data-uid')).to.be('Mx8g7M7hbh')
 		})
 	})
 
@@ -65,7 +73,7 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 			const more = element.shadowRoot.querySelector('.paging > oo-atoms-button')
 			event(more, 'clicked')
 			await sleep(100)
-			const items = element.shadowRoot.querySelectorAll('oo-atoms-message')
+			const items = element.shadowRoot.querySelectorAll('oo-project-card')
 			expect(items).to.have.length(4)
 			expect(element.shadowRoot.querySelector('.paging > oo-atoms-button')).to.not.be.ok()
 		})
