c537b6b77789bcb103ee9372403861a1878cbbfd
aggre <hiroyuki.aggre@gmail.com>
2018-04-09 13:12:14 +0900

    Supports href for cross domain

--- src/lib/tags.ts
+++ src/lib/tags.ts
@@ -1,5 +1,6 @@
 import {html} from './html'
 import {repeat} from 'lit-html/lib/repeat'
+import {href} from './href'
 
 interface Options {
 	className?: string
@@ -7,5 +8,5 @@ interface Options {
 
 export const template = (tags: Array<string>, opts?: Options) => {
 	const {className = 'tags'} = opts || {}
-	return html`<div class$='${className}'>${repeat(tags, tag => html`<span><a href$='/projects/tag/${tag}'>${tag}</a></span>`)}</div>`
+	return html`<div class$='${className}'>${repeat(tags, tag => html`<span><a href$='${href(`/projects/tag/${tag}`)}'>${tag}</a></span>`)}</div>`
 }
