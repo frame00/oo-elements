0f8d8e779a099179357b83b8f780950e47f71de0
aggre <hiroyuki.aggre@gmail.com>
2018-04-20 12:32:20 +0900

    If "type" exists, "type" is returned

--- src/lib/calculate-project-type.ts
+++ src/lib/calculate-project-type.ts
@@ -3,10 +3,14 @@ import {ProjectType} from '../type/oo-project'
 
 const find = (exts: OOExtensions, cond: string) => exts.some(ext => ext.key === cond)
 
+const hasType = (exts: OOExtensions) => find(exts, 'type')
 const hasAssignee = (exts: OOExtensions) => find(exts, 'assignee')
 const hasSkill = (exts: OOExtensions) => find(exts, 'skill')
 
 export default (exts: OOExtensions): ProjectType => {
+	if (hasType(exts)) {
+		return (type => type.value)(exts.find(ext => ext.key === 'type'))
+	}
 	if (hasAssignee(exts)) {
 		return 'ask'
 	}
