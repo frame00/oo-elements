946afa306df207743c1076cf9bf1d4d43e6cca13
aggre <hiroyuki.aggre@gmail.com>
2018-05-01 17:22:43 +0900

    Add test case

--- src/elements/oo-reaction/index.test.ts
+++ src/elements/oo-reaction/index.test.ts
@@ -16,6 +16,34 @@ describe(`<${ELEMENT}></${ELEMENT}>`, () => {
 		expect(getElement(ELEMENT)[0]).to.be.ok()
 	})
 
+	describe('Required attributes', () => {
+		it('Mounted when "data-uid" and "data-type" exist')
+		it('Not mounted when "data-uid" and "data-type" not exist')
+	})
+
+	describe('Show reactions', () => {
+		it('When "data-type" is "upvote", get upvote count from REST API')
+		it('When "data-type" is "join", get join count from REST API')
+		it(
+			'When "data-type" is not "upvote" or "join", get upvote count from REST API'
+		)
+	})
+
+	describe('Control reactions', () => {
+		describe('User is not logged in', () => {
+			it('When user is not logged in, can not upvote')
+			it('When user is not logged in, can not join')
+		})
+		describe('User is not reacted yet', () => {
+			it('When user is logged in and not upvoted yet, can upvote')
+			it('When user is logged in and not joined yet, can join')
+		})
+		describe('User reacted', () => {
+			it('When user is logged in and upvoted, can cancel upvote')
+			it('When user is logged in and joined, can cancel join')
+		})
+	})
+
 	after(() => {
 		removeElement(ELEMENT)
 	})
--- src/elements/oo-reaction/index.ts
+++ src/elements/oo-reaction/index.ts
@@ -77,6 +77,7 @@ export default class extends OOElement {
 		const uid = this.uid
 		const type = this.type
 		const reacted = this.reacted
+		const count = this.count
 		const loggedIn = Boolean(store.uid)
 		if (!uid) {
 			return html``
@@ -93,7 +94,7 @@ export default class extends OOElement {
 				clickHandler()
 					.then()
 					.catch()}'
-					disabled?='${!loggedIn}'>upvote</button>
+					disabled?='${!loggedIn}'>${count}</button>
 		</div>
 		`
 	}
