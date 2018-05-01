997521ae5a4184cbcebed6d630d2fdb92e2cd4f6
aggre <hiroyuki.aggre@gmail.com>
2018-05-01 11:56:35 +0900

    Production is always use https

--- src/conf/api.ts
+++ src/conf/api.ts
@@ -1,4 +1,5 @@
 const { BUILD_MODE, TRAVIS_BRANCH = false } = process.env
 
-export const url = BUILD_MODE === 'TEST' ? '//localhost:3000' : '//api.ooapp.co'
+export const url =
+	BUILD_MODE === 'TEST' ? '//localhost:3000' : 'https://api.ooapp.co'
 export const version = TRAVIS_BRANCH || BUILD_MODE === 'TEST' ? 'stable' : 'dev'
