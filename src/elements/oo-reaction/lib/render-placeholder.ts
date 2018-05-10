3b8f0e51b7a450025bcdfe023b7333b47d6eb149
aggre <hiroyuki.aggre@gmail.com>
2018-05-10 18:49:19 +0900

    Create a template first

--- src/elements/oo-reaction/lib/render-placeholder.ts
+++ src/elements/oo-reaction/lib/render-placeholder.ts
@@ -2,38 +2,39 @@ import { html, render } from '../../../lib/html'
 import style from './style'
 import OOReaction from '..'
 
-export default (el: OOReaction) => {
-	const template = () => {
-		return html`
-		${style()}
-		<style>
-			@import '../../../style/_mixin-button-progress.css';
-			span {
-				display: block;
-				height: 2rem;
-				border-radius: 10px;
-				margin-top: 0.2rem;
-				@mixin progress;
-			}
-			button {
-				@mixin progress;
-				border: 0;
-				&::before {
-					content: '🍣';
-					visibility: hidden;
-				}
-			}
-			[aria-hidden=true] {
+const template = () => {
+	return html`
+	${style()}
+	<style>
+		@import '../../../style/_mixin-button-progress.css';
+		span {
+			display: block;
+			height: 2rem;
+			border-radius: 10px;
+			margin-top: 0.2rem;
+			@mixin progress;
+		}
+		button {
+			@mixin progress;
+			border: 0;
+			&::before {
+				content: '🍣';
 				visibility: hidden;
 			}
-		</style>
-		<label aria-busy=true>
-			<button disabled>
-				<span aria-hidden=true>.</span>
-			</button>
-			<span></span>
-		</label>
-		`
-	}
+		}
+		[aria-hidden=true] {
+			visibility: hidden;
+		}
+	</style>
+	<label aria-busy=true>
+		<button disabled>
+			<span aria-hidden=true>.</span>
+		</button>
+		<span></span>
+	</label>
+	`
+}
+
+export default (el: OOReaction) => {
 	render(template(), el)
 }
--- src/elements/oo-reaction/lib/render-sponsor.ts
+++ src/elements/oo-reaction/lib/render-sponsor.ts
@@ -6,51 +6,52 @@ import style from './style'
 
 define('oo-modal', modal)
 
-export default (el: OOReaction) => {
-	const template = (open: boolean, handler: Function) => {
-		return html`
-		${style()}
-		<style>
-			@import '../../../style/_vars-font-family.css';
+const template = (open: boolean, handler: Function) => {
+	return html`
+	${style()}
+	<style>
+		@import '../../../style/_vars-font-family.css';
 
-			[slot] {
-				padding: 2rem;
+		[slot] {
+			padding: 2rem;
+		}
+		oo-modal {
+			font-family: var(--font-family);
+		}
+		header {
+			background: #4CAF50;
+			h1 {
+				margin: auto;
 			}
-			oo-modal {
-				font-family: var(--font-family);
-			}
-			header {
-				background: #4CAF50;
-				h1 {
-					margin: auto;
+		}
+		main {
+			background: #8BC34A;
+			p {
+				margin: 1rem 0;
+				&:first-child {
+					margin-top: 0;
+				}
+				&:last-child {
+					margin-bottom: 0;
 				}
 			}
-			main {
-				background: #8BC34A;
-				p {
-					margin: 1rem 0;
-					&:first-child {
-						margin-top: 0;
-					}
-					&:last-child {
-						margin-bottom: 0;
-					}
-				}
-			}
-		</style>
-		<label aria-busy=false>
-			<button class=sponsor on-click='${handler}'>-</button>
-			sponsor
-		</label>
-		<oo-modal data-open$='${open ? 'enabled' : 'disabled'}' on-close='${handler}'>
-			<header slot=header><h1>Become a sponsor</h1></header>
-			<main slot=body>
-				<p>This feature is currently in preparation.</p>
-				<p>Contact support via <a href=mailto:support@frame00.com>email</a>(English or Japanese).</p>
-			</main>
-		</oo-modal>
-		`
-	}
+		}
+	</style>
+	<label aria-busy=false>
+		<button class=sponsor on-click='${handler}'>-</button>
+		sponsor
+	</label>
+	<oo-modal data-open$='${open ? 'enabled' : 'disabled'}' on-close='${handler}'>
+		<header slot=header><h1>Become a sponsor</h1></header>
+		<main slot=body>
+			<p>This feature is currently in preparation.</p>
+			<p>Contact support via <a href=mailto:support@frame00.com>email</a>(English or Japanese).</p>
+		</main>
+	</oo-modal>
+	`
+}
+
+export default (el: OOReaction) => {
 	const update = (state: boolean) => () => {
 		render(template(!state, update(!state)), el)
 	}
